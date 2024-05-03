daa7f2f500290a5066ca3902c0f362e5969d68b4
Chad Phillips <chad@apartmentlines.com>
2024-05-03 12:05:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# LLM Workflow Engine (LWE) Chat Fireworks Provider plugin
+
+Chat Fireworks Provider plugin for [LLM Workflow Engine](https://github.com/llm-workflow-engine/llm-workflow-engine)
+
+Access to [Fireworks](https://fireworks.ai/models) chat models.
+
+## Installation
+
+### Export API key
+
+Grab a Fireworks API key from [https://fireworks.ai/api-keys](https://fireworks.ai/api-keys)
+
+Export the key into your local environment:
+
+```bash
+export FIREWORKS_API_KEY=<API_KEY>
+```
+
+### From packages
+
+Install the latest version of this software directly from github with pip:
+
+```bash
+pip install git+https://github.com/llm-workflow-engine/lwe-plugin-provider-chat-fireworks
+```
+
+### From source (recommended for development)
+
+Install the latest version of this software directly from git:
+
+```bash
+git clone https://github.com/llm-workflow-engine/lwe-plugin-provider-chat-fireworks.git
+```
+
+Install the development package:
+
+```bash
+cd lwe-plugin-provider-chat-fireworks
+pip install -e .
+```
+
+## Configuration
+
+Add the following to `config.yaml` in your profile:
+
+```yaml
+plugins:
+  enabled:
+    - provider_chat_fireworks
+    # Any other plugins you want enabled...
+  # THIS IS OPTIONAL -- By default the plugin loads all model data via an API
+  # call on startup. This does make startup time longer, and the CLI completion
+  # for selecting models is very long!
+  # You can instead provide a 'models' object here with the relevant data, and
+  # It will be used instead of an API call.
+  provider_chat_fireworks:
+    models:
+      # 'id' parameter of the model as it appears in the API.
+      # This is also listed on the model's summary page on the OpenRouter
+      # website.
+      "accounts/fireworks/models/llama-v3-8b-instruct":
+        # The only parameter, and it's required.
+        max_tokens: 8192
+```
+
+## Usage
+
+From a running LWE shell:
+
+```
+/provider fireworks
+/model model_name accounts/fireworks/models/llama-v3-8b-instruct
+```
---
+++ lwe_plugin_provider_chat_fireworks/plugin.py
@@ -0,0 +1,87 @@
+import os
+import requests
+
+from langchain_fireworks import ChatFireworks
+
+from lwe.core.provider import Provider, PresetValue
+
+FIREWORKS_API_BASE = "https://api.fireworks.ai/inference/v1"
+
+
+class CustomChatFireworks(ChatFireworks):
+
+    @property
+    def _llm_type(self):
+        """Return type of llm."""
+        return "chat_fireworks"
+
+
+class ProviderChatFireworks(Provider):
+    """
+    Access to Fireworks chat models.
+    """
+
+    def __init__(self, config=None):
+        super().__init__(config)
+        self.models = self.config.get('plugins.provider_chat_fireworks.models') or self.fetch_models()
+
+    def fetch_models(self):
+        models_url = f"{FIREWORKS_API_BASE}/models"
+        try:
+            headers = {
+                "accept": "application/json",
+                "content-type": "application/json",
+                "Authorization": f"Bearer {os.environ['FIREWORKS_API_KEY']}",
+            }
+            response = requests.get(models_url, headers=headers)
+            response.raise_for_status()
+            models_data = response.json()
+            models_list = models_data.get('data')
+            if not models_list:
+                raise ValueError('Could not retrieve models')
+            models = {model['id']: {'max_tokens': model['context_length']} for model in models_list if 'context_length' in model and model['context_length'] and 'supports_chat' in model and model['supports_chat']}
+            return models
+        except requests.exceptions.RequestException as e:
+            raise ValueError(f"Could not retrieve models: {e}")
+
+    @property
+    def default_model(self):
+        pass
+
+    @property
+    def capabilities(self):
+        return {
+            "chat": True,
+            'validate_models': False,
+            "models": self.models,
+        }
+
+    def prepare_messages_method(self):
+        return self.prepare_messages_for_llm_chat
+
+    def llm_factory(self):
+        return CustomChatFireworks
+
+    def customization_config(self):
+        return {
+            "verbose": PresetValue(bool),
+            "model_name": PresetValue(str, options=self.available_models),
+            "temperature": PresetValue(float, min_value=0.0, max_value=2.0),
+            "fireworks_api_base": PresetValue(str, include_none=True),
+            "fireworks_api_key": PresetValue(str, include_none=True, private=True),
+            "request_timeout": PresetValue(int),
+            "n": PresetValue(int, 1, 10),
+            "max_tokens": PresetValue(int, include_none=True),
+            "model_kwargs": {
+                "top_p": PresetValue(float, min_value=0.0, max_value=1.0),
+                'top_k': PresetValue(int, min_value=1, max_value=40),
+                "presence_penalty": PresetValue(float, min_value=-2.0, max_value=2.0),
+                "frequency_penalty": PresetValue(float, min_value=-2.0, max_value=2.0),
+                "logit_bias": dict,
+                "logprobs": PresetValue(bool, include_none=True),
+                "top_logprobs": PresetValue(int, min_value=0, max_value=20, include_none=True),
+                "response_format": dict,
+                "stop": PresetValue(str, include_none=True),
+                "user": PresetValue(str),
+            },
+        }
